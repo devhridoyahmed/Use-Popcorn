64dffd328cdc1c32c17ed7afd6509d5752fe2064
Hridoy Ahmed <hridoyahmed47@gmail.com>
2024-08-16 09:51:04 +0600

    star rating api reusability updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="powderblue"
+        maxRating={10}
+        onSetRating={setMovieRating}
+      />
+
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "ok", "good", "amazing"]}
+      defaultRating={3}
+    />
     <StarRating maxRating={20} />
-    <StarRating />
+    <StarRating size={100} color="blue" />
+    <Test />
   </React.StrictMode>
 );
