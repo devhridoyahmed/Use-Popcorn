0b3edc1e76fcd9d435f7c3c042c6b319d7ffeb7d
Hridoy Ahmed <hridoyahmed47@gmail.com>
2024-12-30 10:10:17 +0600

    using cleanup function to reset effect hook result into default. Also implemented browser api to to cancel unnecessary api call to omdb server and getting working tree

--- src/App.js
+++ src/App.js
@@ -92,12 +92,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -109,7 +112,10 @@ export default function App() {
           setMovies(data.Search);
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -120,6 +126,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
@@ -369,6 +379,11 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = `usePorcorn`;
+        console.log(`clean up effect for movie ${title}`);
+      };
     },
     [title]
   );
@@ -452,7 +467,7 @@ function WatchedMovie({ movie, onDeleteMovie }) {
       <div>
         <p>
           <span>⭐️</span>
-          <span>{movie.imdbID}</span>
+          <span>{movie.imdbRating}</span>
         </p>
         <p>
           <span>🌟</span>
